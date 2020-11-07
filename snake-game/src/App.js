68d0af253b912744341c12bdaadd4aa4eb1af6be
ms-online <27732357@qq.com>
2020-11-07 16:55:06 +0800

    lesson-8

--- snake-game/src/App.js
+++ snake-game/src/App.js
@@ -14,12 +14,67 @@ const getRandomCoordinates = () =>{
 class App extends Component {
   state = {
     food:getRandomCoordinates(),
+    direction:'RIGHT',
+    speed:200,
     snakeDots: [
       [0, 0],
       [2,0]
     ]
   }
- 
+ //1键盘事件
+ componentDidMount() {
+   setInterval(this.moveSnake,this.state.speed );
+   document.onkeydown = this.onkeydown;
+ }
+ //2键盘函数
+ onkeydown =(e) => {
+   switch (e.keyCode) {
+      case 38:
+        this.setState({ direction: 'UP'});
+        break;
+      case 40:
+        this.setState({ direction: 'DOWN'});
+        break;
+      case 37:
+        this.setState({ direction: 'LEFT'});
+        break;
+      case 39:
+        this.setState({ direction: 'RIGHT'});
+        break;
+      default:
+        this.setState({ direction: 'RIGHT'});
+   }
+ }
+
+ //3移动贪吃蛇函数
+ moveSnake = () => {
+   let dots = [...this.state.snakeDots];
+   let head = dots[dots.length - 1];
+
+   switch (this.state.direction) {
+     case 'RIGHT':
+       head = [head[0] + 2, head[1]];
+       break;
+     case 'LEFT':
+       head = [head[0] - 2, head[1]];
+      break;
+     case 'DOWN':
+       head = [head[0], head[1] + 2];
+      break;
+      case 'UP':
+      head = [head[0], head[1] - 2];
+      break;
+     default:
+       break;
+   }
+
+   dots.push(head);
+   dots.shift();
+   this.setState({
+     snakeDots:dots
+   })
+ }
+
   render() {
     return (
       <div className='game-area'>
