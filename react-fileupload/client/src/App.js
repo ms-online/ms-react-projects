4fa26c66532630d2e30d0e94b7dd1490f47ae497
ms-online <27732357@qq.com>
2020-11-08 17:58:28 +0800

    lesson-11

---
+++ react-fileupload/client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h4 className="display-4 text-center mb-4">
+        <i className="fab fa-react"></i>React 文件上传
+      </h4>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-fileupload/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+
+const app = express();
+
+//使用中间件
+app.use(fileUpload());
+
+//处理路由
+app.post('/upload', (req, res) => {
+    //判断files属性有没有文件
+    if (req.files === null) {
+        return res.status(400).json({msg:'没有上传的文件！'})
+    }
+
+    const file = req.files.file;
+
+    //将文件移动到指定位置
+    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send(err);
+        }
+        res.json({
+            fileName: file.name,
+            filePath:`/upload/${file.name}`
+        })
+    })
+})
+
+//监听端口号
+app.listen(5001, () => {
+    console.log('服务器正在5001端口号运行...');
+});
