85f9f58c717904038280466bf43dec3ff19fcaa1
ms-online <27732357@qq.com>
2020-11-11 15:13:03 +0800

    lesson-29

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -1,40 +1,40 @@
-import React , { useState }from 'react';
+import React,{ useState } from 'react'
 import Values from 'values.js'
 
 function App() {
    
-  //初始化保存的state
+  //初始化state值
   const [color, setColor] = useState('');
-  const [list, setList] = useState(new Values('#f15025').all(10));
+  const [list, setList] = useState([]);
   const [error, setError] = useState(false);
-
+  
+  //提交函数
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10)
-      setList(colors)
-      setError(false)
-      console.log(color,list)
+      setList(colors);
+      setError(false);
     } catch (error) {
-      setError(true)
+      setError(true);
       console.log(error);
     }
     
-    
-  }
-  
 
+  }
+
+  console.log(list);
   return (
     <>
-      <section className="container">
+    <section className="container">
         <h3>颜色生成器</h3>
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="#f15025" onChange={(e) => setColor(e.target.value)} className={ `${error ? 'error' : null}`}/>
-          <button className="btn" type="submit" >提交</button>
+          <input type="text" placeholder="#f15025" onChange={(e) => setColor(e.target.value)} className={ `${error ? 'error' : null}` }/>
+          <button type="submit" className="btn">提交</button>
         </form>
-      </section>
-      <section>
-          颜色列表
+    </section>
+    <section>
+     颜色列表
       </section>
     </>
   );
