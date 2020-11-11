6766ac2aaf1f9e249986394b888e19a0a215fe2f
ms-online <27732357@qq.com>
2020-11-11 18:47:38 +0800

    lesson-31

---
+++ slider-react/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data'
+function App() {
+  //初始化state值
+  const [people, setPeople] = useState(data)
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>评价信息
+        </h2>
+      </div>
+      {/* 详情 */}
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person
+          
+          //样式
+          return (
+            <article  key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title }</p>
+              <p className="title">{quote}</p>
+              <FaQuoteRight className="icon"/>
+            </article>
+          )
+        })}
+        <button className="prev" >
+          <FiChevronLeft/>
+        </button>
+        <button className="next" >
+          <FiChevronRight/>
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
