5012af3d3439aa2df69fef21d8bbcd3cf70ced5a
ms-online <27732357@qq.com>
2020-11-11 19:17:58 +0800

    lesson-32

--- slider-react/src/App.js
+++ slider-react/src/App.js
@@ -1,10 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data'
 function App() {
   //初始化state值
   const [people, setPeople] = useState(data)
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1
+    if (index < 0) {
+      setIndex(lastIndex)
+    }
+    if (index > lastIndex) {
+      setIndex(0)
+    }
+  }, [index, people]);
+
+  //自动播放
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1)
+    }, 5000);
+    return ()=> clearInterval(slider)
+  }, [index]);
+
   return (
     <section className="section">
       <div className="title">
@@ -16,10 +36,18 @@ function App() {
       <div className="section-center">
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person
-          
+          //动态添加class属性
+          let position = 'nextSlide';
+          if (personIndex === index) {
+            position = 'activeSlide';
+          }
+          if (personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
+              position ='lastSlide';
+          };
+
           //样式
           return (
-            <article  key={id}>
+            <article  className={position} key={id}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title }</p>
@@ -28,10 +56,10 @@ function App() {
             </article>
           )
         })}
-        <button className="prev" >
+        <button className="prev"  onClick={()=> setIndex(index - 1)}>
           <FiChevronLeft/>
         </button>
-        <button className="next" >
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight/>
         </button>
       </div>
