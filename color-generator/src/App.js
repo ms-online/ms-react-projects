a0e1bad77b92d832aa45603eb7fd8b8ab6e193da
ms-online <27732357@qq.com>
2020-11-11 14:16:54 +0800

    lesson-29

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -1,10 +1,42 @@
-
+import React , { useState }from 'react';
+import Values from 'values.js'
 
 function App() {
+   
+  //初始化保存的state
+  const [color, setColor] = useState('');
+  const [list, setList] = useState(new Values('#f15025').all(10));
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors)
+      setError(false)
+      console.log(color,list)
+    } catch (error) {
+      setError(true)
+      console.log(error);
+    }
+    
+    
+  }
+  
+
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <section className="container">
+        <h3>颜色生成器</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="#f15025" onChange={(e) => setColor(e.target.value)} className={ `${error ? 'error' : null}`}/>
+          <button className="btn" type="submit" >提交</button>
+        </form>
+      </section>
+      <section>
+          颜色列表
+      </section>
+    </>
   );
 }
 
